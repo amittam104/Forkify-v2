3ba2aa5cd11ad0a355c1434bd7c0a2ee866b5f4a
Amit Tambulkar <amittambulkar104@gmail.com>
2024-05-11 16:51:46 +0530

    feat: add number of pages between pagination buttons

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -1,12 +1,12 @@
-import View from './View.js';
-import icons from 'url:../../img/icons.svg';
+import View from "./View.js";
+import icons from "url:../../img/icons.svg";
 
 class PaginationView extends View {
-  _parentElement = document.querySelector('.pagination');
+  _parentElement = document.querySelector(".pagination");
 
   addHandlerPagination(handler) {
-    this._parentElement.addEventListener('click', function (e) {
-      const btn = e.target.closest('.btn--inline');
+    this._parentElement.addEventListener("click", function (e) {
+      const btn = e.target.closest(".btn--inline");
 
       if (!btn) return;
 
@@ -22,19 +22,21 @@ class PaginationView extends View {
     // console.log(numPages, currPage);
 
     // 1. Current Page 1 and there are other pages
-    if (currPage === 1 && numPages > 1) return this._forwardBtnMarkup();
+    if (currPage === 1 && numPages > 1) return this._forwardBtnMarkup(numPages);
 
     // 2. On other pages
-    if (currPage > 1 && currPage < numPages) return this._bothBtnMarkup();
+    if (currPage > 1 && currPage < numPages)
+      return this._bothBtnMarkup(numPages);
 
     // 3. On last page and earlier pages are there
-    if (currPage === numPages && numPages > 1) return this._backwardBtnMarkup();
+    if (currPage === numPages && numPages > 1)
+      return this._backwardBtnMarkup(numPages);
 
     // 4. Only 1 Page
-    return '';
+    return "";
   }
 
-  _backwardBtnMarkup() {
+  _backwardBtnMarkup(numPages) {
     return `
         <button data-goto="${
           this._data.page - 1
@@ -43,12 +45,30 @@ class PaginationView extends View {
             <use href="${icons}#icon-arrow-left"></use>
           </svg>
           <span>Page ${this._data.page - 1}</span>
-        </button>  
+        </button> 
+        <button disabled class="page-numbers">
+          <span>${numPages} pages</span>
+        </button>
+        <button disabled class="btn--inline--hidden opac">
+          <span>Page ${this._data.page + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
       `;
   }
 
-  _forwardBtnMarkup() {
+  _forwardBtnMarkup(numPages) {
     return `
+      <button disabled class="btn--inline--hidden opac">
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-left"></use>
+        </svg>
+        <span>Page ${this._data.page - 1}</span>
+      </button> 
+      <button disabled class="page-numbers">
+        <span>${numPages} pages</span>
+      </button>
       <button data-goto="${
         this._data.page + 1
       }" class="btn--inline pagination__btn--next">
@@ -60,7 +80,7 @@ class PaginationView extends View {
     `;
   }
 
-  _bothBtnMarkup() {
+  _bothBtnMarkup(numPages) {
     return `
       <button data-goto="${
         this._data.page - 1
@@ -69,6 +89,9 @@ class PaginationView extends View {
           <use href="${icons}#icon-arrow-left"></use>
         </svg>
         <span>Page ${this._data.page - 1}</span>
+      </button>
+      <button class="page-numbers">
+        <span>${numPages} pages</span>
       </button>
       <button data-goto="${
         this._data.page + 1
